66f6b9d39e6c6f649e8704621537c35019c14f0a
shinpei <shinpei.nakata@gmail.com>
2019-12-28 23:11:27 +0900

    ES compat

--- golr.go
+++ golr.go
@@ -3,7 +3,6 @@ package golr
 import (
 	"bytes"
 	"encoding/json"
-	"encoding/xml"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -13,17 +12,43 @@ import (
 	"sync"
 )
 
+
+type ConnectorBase struct {
+	url string
+}
 type SolrConnector struct {
-	host string
-	port int
+	ConnectorBase
+}
+
+type ESConnector struct {
+	ConnectorBase
 }
 
 type SolrAddOption struct {
 	Concurrency int
+	addressTemplate string
 }
 
-func Connect(host string, port int) *SolrConnector {
-	return &SolrConnector{host, port}
+func (base *ConnectorBase) Connect (url string){
+	base.url = url
+}
+
+type Connector interface {
+	Connect (url string)
+	PostUpdate(payload []byte) ([]byte, error)
+	AddDocuments(container interface{}, opt *SolrAddOption) <-chan []byte
+}
+
+func NewSolrConnect(host string,port int) *SolrConnector {
+	c := &SolrConnector{}
+	c.Connect(fmt.Sprintf("http://%s:%d/solr/update/json", host, port))
+	return c;
+}
+
+func NewESConnect ( host string, port int, collection string) *ESConnector {
+	e := &ESConnector{}
+	e.Connect(fmt.Sprintf("http://%s:%d/%s/_bulk", host, port, collection))
+	return e
 }
 
 // Assumes it'll get arrays of some data structure
@@ -55,8 +80,7 @@ func (sc *SolrConnector) AddDocuments(container interface{}, opt *SolrAddOption)
 func (sc *SolrConnector) PostUpdate(payload []byte) ([]byte, error) {
 
 	client := &http.Client{}
-	url := fmt.Sprintf("http://%s:%d/solr/update/json", sc.host, sc.port)
-	req, err := http.NewRequest("POST", url, bytes.NewReader(payload))
+	req, err := http.NewRequest("POST", sc.url, bytes.NewReader(payload))
 	req.Header.Add("Content-type", "application/json")
 
 	//dump, _ := httputil.DumpRequestOut(req, true)
@@ -71,40 +95,56 @@ func (sc *SolrConnector) PostUpdate(payload []byte) ([]byte, error) {
 		return nil, err
 	}
 	log.Printf("Recieved %d bytes.\n", len(body))
+	log.Printf("%s\n", body)
 	return body, nil
 }
 
-type XMLProcessor interface {
-	Process(inputChan chan interface{}, opt *SolrAddOption, decoder *xml.Decoder)
+// Assumes it'll get arrays of some data structure
+func (sc *ESConnector) AddDocuments(container interface{}, opt *SolrAddOption) <-chan []byte {
+	recvChan := make(chan []byte)
+
+	var err error
+	// todo: size constrain should be placed here
+	defer func() {
+		if err != nil {
+			log.Printf("Error occured, uploading document failed")
+		}
+	}()
+	go func(rC chan []byte) {
+		b, err := json.Marshal(container)
+		if err != nil {
+			log.Println("Failed at marshaling json structure, ", err)
+		}
+
+		respB, err := sc.PostUpdate(b)
+		if err != nil {
+			log.Println(err)
+		}
+		rC <- respB
+	}(recvChan)
+	return recvChan
 }
 
-func (sc *SolrConnector) UploadXMLFile(
-	path string,
-	processor XMLProcessor,
-	opt *SolrAddOption) {
-	xmlFile, err := os.Open(path)
+func (sc *ESConnector) PostUpdate(payload []byte) ([]byte, error) {
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", sc.url, bytes.NewReader(payload))
+	req.Header.Add("Content-type", "application/json")
+
+	//dump, _ := httputil.DumpRequestOut(req, true)
+	//fmt.Printf("%s", dump)
+	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("Error opening file:", err)
-		return
+		return nil, err
 	}
-
-	defer xmlFile.Close()
-
-	// TODO: is this thread(goroutine) safe?
-	decoder := xml.NewDecoder(xmlFile)
-
-	// prepare goroutines
-	wg := new(sync.WaitGroup)
-	inputChan := make(chan interface{})
-	for i := 0; i < opt.Concurrency; i++ {
-		wg.Add(1)
-		go golrworker(sc, inputChan, opt, wg)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
 	}
-
-	processor.Process(inputChan, opt, decoder)
-
-	close(inputChan)
-	wg.Wait()
+	//log.Printf("Recieved %d bytes.\n", len(body))
+	//log.Printf("%s\n", body)
+	return body, nil
 }
 
 //TODO: Fix argument type
@@ -131,10 +171,10 @@ func (sc *SolrConnector) UploadJSONFile(path string, processor JSONProcessor, op
 	wg.Wait()
 }
 
-func golrworker(sc *SolrConnector, inputChan chan interface{}, opt *SolrAddOption, wg *sync.WaitGroup) {
+func golrworker(c Connector, inputChan chan interface{}, opt *SolrAddOption, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for pages := range inputChan {
-		msg := <-sc.AddDocuments(pages, opt)
+		msg := <-c.AddDocuments(pages, opt)
 		fmt.Println(string(msg[:]))
 	}
 }
